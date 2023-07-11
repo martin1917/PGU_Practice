7028d0dd5b23caf9e4c8f9eb44be7f80797e8f14
martin1917 <martin.levin02@mail.ru>
2023-07-11 20:54:31 +0300

    del returning type

--- Laba2/main.py
+++ Laba2/main.py
@@ -3,7 +3,7 @@ from HyperbolicFunctions import HyperbolicSine, HyperbolicCosine
 from SingleVariableFunc import SingleVariableFunc
 
 
-def calc(func: SingleVariableFunc, x: float) -> tuple[float, float]:
+def calc(func: SingleVariableFunc, x: float):
     print(f'ФУНКЦИЯ: {func.name}\n')
 
     print(f'Значения в точке x = {x}', func.calc(x), sep='\n')
